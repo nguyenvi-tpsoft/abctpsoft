d03596f8d958e0cafd5ad131d2c571a14e636a46
Khánh Vi Nguyễn <vicntt10@gmail.com>
2022-02-14 14:43:20 +0700

    abc

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.responsivevoice.org/responsivevoice.js?key=QewEgAhc"></script>
 </head>
 
 <body>
-    <p>hello</p>
-    <script src="https://code.responsivevoice.org/responsivevoice.js?key=QewEgAhc"></script>
+    <input type="text" id="text_input">
+    <button onclick="responsiveVoice.speak('The Internet is a series of tubes!');" type="button" value="Play">Play</button>
     <script>
-        responsiveVoice.speak("hello world");
+        var textinput = $('#text_input').val();
+        responsiveVoice.speak(textinput);
     </script>
 </body>
 
